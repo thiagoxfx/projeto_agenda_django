993d1fa0658443a8f0ad1e060d899e1f14cb756b
Thiago Guimaraes <thiagoxfx@gmail.com>
2025-08-09 21:50:13 -0300

    criação do model contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# id (primary key - automático)
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+
+# Depois
+# category (foreign key), show (boolean), owner (foreign key)
+# picture (imagem)
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    create_date = models.DateField(default=timezone.now)
+    description = models.TextField(blank=True)

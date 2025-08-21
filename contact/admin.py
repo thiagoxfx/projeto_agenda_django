05ca15eab911da9ed1fd3211a1a64dcd9c542c16
Thiago Guimaraes <thiagoxfx@gmail.com>
2025-08-20 23:24:44 -0300

    modificado READ.md e admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,14 @@
-from django.contrib import admin
 from contact import models
+from django.contrib import admin
+
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    ordering = ('id',)
+    search_fields = ('id', 'first_name', 'last_name',)
+    # list_filter = ('create_date',)
+    list_per_page = 1
+    list_max_show_all = 200
+    list_editable = ('first_name', 'last_name',)
+    list_display_links = ('id', 'phone',)

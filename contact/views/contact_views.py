ed5d68d9a6fe3521321a1293b8f9ea7d17df3957
Thiago Guimaraes <thiagoxfx@gmail.com>
2025-08-25 23:53:54 -0300

    criação dos partials

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,4 @@
 from contact.models import Contact
-from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
 
@@ -7,6 +6,7 @@ def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - ',
     }
 
     return render(
@@ -21,9 +21,11 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(

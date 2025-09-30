ed677d6ea9c41e8cac50796a61f031ca397915ba
Thiago Guimaraes <thiagoxfx@gmail.com>
2025-09-30 00:24:57 -0300

    inclusão de fotos no perfil

--- contact/urls.py
+++ contact/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
 
     path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
     path('contact/create/', views.create, name='create'),
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,6 +1,6 @@
 from contact.forms import ContactForm
 from contact.models import Contact
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
 
@@ -8,7 +8,7 @@ def create(request):
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES,)
         context = {
             'form': form,
             'form_action': form_action,
@@ -41,7 +41,7 @@ def update(request, contact_id):
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
         context = {
             'form': form,
             'form_action': form_action,
@@ -66,4 +66,24 @@ def update(request, contact_id):
         request,
         'contact/create.html',
         context
+    )
+
+
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
     )

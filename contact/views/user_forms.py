c8846c87fa4046e83c270d479ba66835ba36a016
Thiago Guimaraes <thiagoxfx@gmail.com>
2025-10-04 21:54:55 -0300

    inclusão de mensagens na criação de usuario e vefificações

---
+++ contact/views/user_forms.py
@@ -0,0 +1,24 @@
+from contact.forms import RegisterForm
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+
+def register(request):
+    form = RegisterForm()
+
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Usuario registrado com sucesso')
+            return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/register.html',
+        {
+            'form': form
+        }
+    )
